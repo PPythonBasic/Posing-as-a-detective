ee2ab58086e10b8882f5a6f03c718936d163a010
aumjw <jutabodeeaum@gmail.com>
2023-09-30 21:12:54 +0700

    choice 1 finish

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ from posing_a_detective import run_code
 
 if __name__ == "__main__":
     run_code()
-
- git config --global user.email "jutabodeeaum@gmail.com"
-  git config --global user.name "aumjw"
