dca673fa35c74019e3082cf54494f933fe7b525f
aumjw <jutabodeeaum@gmail.com>
2023-09-30 20:31:20 +0700

    Add comments in main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from posing_a_detective import run_code
 
 if __name__ == "__main__":
     run_code()
+
+ git config --global user.email "jutabodeeaum@gmail.com"
+  git config --global user.name "aumjw"
