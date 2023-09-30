32ec3975fa43ad4efe502415cf138b354728c66b
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-09-30 08:57:20 +0000

    Update code

--- posing_a_detective.py
+++ posing_a_detective.py
@@ -4,18 +4,13 @@ except:
     import os
     os.system('pip install requests')
 
-import random
 import json
 
 
 def pull_database_suspect():
     """
-    ฟังก์ชันดึงข้อมูลจากฐานข้อมูลผู้ต้องสงสัย API
+    ฟังก์ชันดึงข้อมูลจากฐานข้อมูลผู้ต้องสงสัย API https://ppythonbasic.github.io/apis/suspects.json
     """
-    url = 'https://ppythonbasic.github.io/apis/suspects.json'
-    response = requests.get(url)
-    data_suspcet = json.loads(response.text)
-    print(data_suspcet['suspects'])
 
 
 def run_code():
